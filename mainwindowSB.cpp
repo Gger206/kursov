cb0b574387a479e78b40bb59655a8e6de28b159c
Gger206 <arteml22012006@gmail.com>
2025-05-27 23:41:16 +0300

    game field create

--- mainwindowSB.cpp
+++ mainwindowSB.cpp
@@ -1,14 +1,30 @@
-#include "mainwindow.h"
-#include "./ui_mainwindowSB.h"
+#include "mainwindowSB.h"
+#include "ui_mainwindowSB.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    setupPlayerField();
+    setupEnemyField();
+
+    connect(ui->startButton, &QPushButton::clicked, this, &MainWindow::onStartGame);
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
     delete ui;
+}
+
+void MainWindow::setupPlayerField() {
+    m_playerField = new GameField(this, ui->widget_3);
+}
+
+void MainWindow::setupEnemyField() {
+    m_enemyField = new GameField(this, ui->_2);
+}
+
+void MainWindow::onStartGame() {
+    ui->label->setText("Игра началась!");
 }
